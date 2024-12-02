259e9edbfc3e918b008d942b4b590e8c18b9507a
Rachel Marquant <marquantrachel@gmail.com>
2024-12-02 16:11:06 +0100

    Ajout du bouton de suppression

--- js/main.js
+++ js/main.js
@@ -1,26 +1,36 @@
-console.log("J'ai reussi a lier Js au fichier Html!");
-
-const addBtn = document.querySelector('#btn');
+const addBtn = document.querySelector('#btn'); // c'est le premier élément  #btn que la function trouve dans le html
 const taskCard = document.querySelector('.todoCard');
 const taskContainer = document.querySelector('#todoCards');
 
+const delBtn = document.querySelector('.delBtn'); // on recupere le premier .delBtn
+delBtn.addEventListener('click', function() {
+    deleteTask(taskCard);
+    });
 
-
-
-
-// const addBtn = document.querySelector('#btn');
 addBtn.addEventListener('click', addTask); // pour ajouter une tache
 
-
-
-
-
-
-
+// on crée la fonction d'ajout
 function addTask(){
-    const newTask = taskCard.cloneNode(true);
-    const newsTextArea = newTask.querySelector('.task');
-    newsTextArea.value = "New Task";
-    taskContainer.appendChild(newTask);
+    const newTask = taskCard.cloneNode(true); // on clone l'élement NewTask
+    const newDelBtn = newTask.querySelector('.delBtn');
+    const newTextArea = newTask.querySelector('.task'); 
+
+    newTextArea.value = "New Task"; // changement de variable pour qu'elle ne soit pas vide
+    newDelBtn.addEventListener('click',function(){
+        deleteTask(newTask);
+    });
+
+    taskContainer.appendChild(newTask); // ajoute un enfant à taskContainer et permet la duplication
+    updateCount();
 }
+
+
+// on créer la fonction de suppression
+function deleteTask(task){
+    task.remove();
+}
+
+
+
+
 
