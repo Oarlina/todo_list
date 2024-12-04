30a57b711d50828092c3ebb627d467b2ef6be086
Rachel Marquant <marquantrachel@gmail.com>
2024-12-04 14:43:56 +0100

    Ajout de tâche réalisé et déplacement dans "tâche finie"

--- js/main.js
+++ js/main.js
@@ -3,39 +3,50 @@ const taskCard = document.querySelector('.todoCard');
 const taskContainer = document.querySelector('#todoCards');
 const taskCard2 = document.querySelector('.task');
 const taskContainer2 = document.querySelector('#todoCardsFinish');
+const delBtn = document.querySelector('.delBtn'); // on recupere le premier .delBtn
+const checkBtn = document.querySelector('.checkBtn'); // on recupere le premier .checkBtn
 let nombre =1;
 let nombrefini=0;
 let probleme = "Veuillez ajouter une tache avant de valider/supprimer";
 document.getElementById("compterlist").innerHTML = nombre;
 document.getElementById("compternb").innerHTML = nombrefini;
-const delBtn = document.querySelector('.delBtn'); // on recupere le premier .delBtn
-const checkBtn = document.querySelector('.checkBtn'); // on recupere le premier .checkBtn
+
+
+addBtn.addEventListener('click', addTask); // pour ajouter une tache
 
 delBtn.addEventListener('click', function() {
     deleteTask(taskCard);
     });
 
-addBtn.addEventListener('click', addTask); // pour ajouter une tache
-
 checkBtn.addEventListener('click', function() {
     deplaceTask(taskCard);
     });
+    
 
 
 // on crée la fonction qui validera une tache
-function deplaceTask(){
-    const newTask2 = taskCard.cloneNode(true); // on clone l'élement NewTask
-    const newDelBtn2 = newTask2.querySelector('.delBtn');
-    const newTextArea2 = newTask2.querySelector('.task'); 
-    taskContainer2.appendChild(newTextArea2); // ajoute un enfant à taskContainer et permet la duplication
-    nombrefini++; // pour ajouter un au compteur
-    newDelBtn2.addEventListener('click',function(){
-        deleteTask(newTask2);
-        updateCount(); // permet de mettre a jour le compteur
-    });
-    newTextArea2.value = "New Task"; // changement de variable pour qu'elle ne soit pas vide
-    deleteTask(taskCard); // pour supprimer la liste que je valide
-    updateCount(); 
+function deplaceTask(task){
+    // si nomlbre de tache = 1, ne peux pas deplacer, sinon peut deplacer
+    if (nombre>1){
+        const newTask2 = task.cloneNode(true); // on clone l'élement NewTask
+        const newDelBtn2 = newTask2.querySelector('.delBtn');
+        const newTextArea2 = newTask2.querySelector('.task'); 
+        
+        taskContainer2.appendChild(newTextArea2); // ajoute un enfant à taskContainer et permet la duplication
+        nombrefini++; // pour ajouter un au compteur
+        
+        newDelBtn2.addEventListener('click',function(){
+            deleteTask(newTask);
+            updateCount(); // permet de mettre a jour le compteur
+        });
+        
+        newTextArea2.value = "New Task"; // changement de variable pour qu'elle ne soit pas vide
+        deleteTask(task); // pour supprimer la liste que je valide
+        // deleteTask(task);
+        updateCount(); 
+    } else {
+        document.querySelector(".probleme").innerHTML = probleme;
+    }
 }
 
 
@@ -43,13 +54,21 @@ function deplaceTask(){
 function addTask(){
     const newTask = taskCard.cloneNode(true); // on clone l'élement NewTask
     const newDelBtn = newTask.querySelector('.delBtn');
+    const newCheckBtn = newTask.querySelector('.checkBtn');
     const newTextArea = newTask.querySelector('.task'); 
+
     nombre++; // pour ajouter un au compteur
     newTextArea.value = "New Task"; // changement de variable pour qu'elle ne soit pas vide
+
     newDelBtn.addEventListener('click',function(){
         deleteTask(newTask);
         updateCount(); // permet de mettre a jour le compteur
     });
+
+    newCheckBtn.addEventListener('click', function() {
+        deplaceTask(newTask);
+        });
+
     taskContainer.appendChild(newTask); // ajoute un enfant à taskContainer et permet la duplication
     updateCount(); 
 }
@@ -66,6 +85,7 @@ function deleteTask(task){
     if (nombre>1){ // pour qu'il reste forcément une liste
     task.remove();
     nombre--; // pour retirer un au compteur
+
     }else if (nombre == 1)
     {
         document.querySelector(".probleme").innerHTML = probleme;
